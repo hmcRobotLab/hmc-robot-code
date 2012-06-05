218ad9cd6a2212307ed393e73e2a753af719ed87
marble@robotLab <marble@robotLab>
2012-06-05 13:30:44 -0700

    Adding image processor

---
+++ images/imagePub/src/my_publisher.cpp
@@ -0,0 +1,23 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv/cvwimage.h>
+#include <opencv/highgui.h>
+#include <cv_bridge/CvBridge.h>
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "image_publisher");
+  ros::NodeHandle nh;
+  image_transport::ImageTransport it(nh);
+  image_transport::Publisher pub = it.advertise("camera/image", 1);
+
+  cv::WImageBuffer3_b image( cvLoadImage(argv[1], CV_LOAD_IMAGE_COLOR) );
+  sensor_msgs::ImagePtr msg = sensor_msgs::CvBridge::cvToImgMsg(image.Ipl(), "bgr8");
+  
+  ros::Rate loop_rate(5);
+  while (nh.ok()) {
+    pub.publish(msg);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+}
