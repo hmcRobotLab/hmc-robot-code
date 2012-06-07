592dc41c191a5717d63f20dac0941c30c9b70123
arnold@robotlab <arnold@robotlab>
2012-06-07 12:46:20 -0700

    Working on ardrone, all that is left is parsing commands and debugging

--- ardrone_emulator/nodes/ardrone_emulator.py
+++ ardrone_emulator/nodes/ardrone_emulator.py
@@ -1,13 +1,20 @@
-import roslib; roslib.load_manifest('image_transport')
+import roslib; roslib.load_manifest('ardrone_emulator')
 
 import rospy
-import sys
+import sys, time
+import cv
 
-
+from ardrone_emulator.msg import NavData
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
 class DroneEmulator:
 
-    def __init__(self, imageDir, xMax, yMax, zMax, imScale, numHours = 12, location = (0,0), image_hour = 3):
+    def __init__(self, imageDir, xMax, yMax, zMax, imScale, numHours = 12, location = (0,0), image_hour = 0, delay_time = 1):
+
+        #Right now this is totally implausible as we do everything discretely, so we say that a command to go forward,
+        # regardless of speed, sends the robot forward at a rate of imScale/delayTime
+        self.delayTime = delay_time
 
         self.xMax = xMax
         self.yMax = yMax
@@ -21,70 +28,33 @@ class DroneEmulator:
 
         self.image_hour = image_hour # heading at 3 o'clock == toward the markers
 
-        self.last_image_time = time.time() # last time an image was grabbed
+        self.vel = (0,0,0,0)
+
+        self.lastUpdateTime = time.time() # last time we updated our image
         self.baseImageDir = imageDir
 
-    def change_location(self,direction):
-        """ changes the location from which an image is taken in one of six ways """
-        # get the state
-        current_ns_number = self.location[0]
-        current_ew_number = self.location[1]
-        current_image_hour = self.image_hour
+        self.navPublisher = rospy.Publisher('navData', NavData)
+        self.imagePublisher = rospy.Publisher('droneImage', Image)
 
-        # make appropriate changes
-        if direction == 'west':
-            current_ew_number += 1       
-            if current_ew_number > 6: current_ew_number = self.maxWest
-        elif direction == 'east':
-            current_ew_number -= 1
-            if current_ew_number < 0: current_ew_number = 0
-        elif direction == 'north':
-            current_ns_number += 1
-            if current_ns_number < 2: current_ns_number = self.maxNorth
-        elif direction == 'south':
-            current_ns_number -= 1
-            if current_ns_number < 0: current_ns_number = 0
-        elif direction == 'counterclockwise':
-            current_image_hour = (current_image_hour - 1) % self.numHours
-        elif direction == 'clockwise':
-            current_image_hour = (current_image_hour + 1) % self.numHours
-        else:
-            print "a direction of", direction,
-            print "was not recognized in change_location"
+        self.publishImage()
 
-        # reset the state
-        self.location = (current_ns_character,current_ew_number)
-        self.image_hour = current_image_hour
-        print "location and image_hour are now", self.location, self.image_hour
+        #Setting up the navdata message
+        self.navData = NavData()
 
+        #These parameters never change
+        self.navData.batLevel = 100
+        self.navData.tag = 0
+        self.navData.size = 0
+        self.navData.ctrlState = 0
+        self.navData.numFrames = 0
+        self.navData.theta = 0 #This part is not realistic. If we wanted too, we could record a fake theta based on speed.
+        self.navData.phi = 0 #Likewise not realistic.
 
-    def keyControls(self,c):
-       # if not self.use_drone (simulated mode)
-       # the arrow keys allow you to change the images you see...
-       if not self.use_drone:
-           if c == 82:  # up arrow is the same as ord('R')
-               self.change_location( 'west' )
-           if c == 84:  # down arrow is the same as ord('T')
-               self.change_location( 'east' )
-           if c == 81:  # left arrow is the same as ord('Q')
-               self.change_location( 'north' )
-           if c == 83:  # right arrow is the same as ord('S')
-               self.change_location( 'south' )
-           if c == ord('-'):  # '-' decreases the image_hour
-               self.change_location( 'counterclockwise' )
-           if c == ord('=') or c == ord('+'):  # '=' or '+' increases the image_hour
-               self.change_location( 'clockwise' )
+        #Now we get the variable ones and publish it. 
+        self.publishNavData()
 
-    def updateImage(self, data):
-        cur_time = time.time()
-        if cur_time - self.last_image_time > 0.25: # number of seconds per update
-            self.last_image_time = cur_time # reset the last image time
-            folder = self.folder_names[self.location]
-            fn = folder + "/" + str(self.image_hour) + ".png"
-            #print "filename", fn
-            self.color_image=cv.LoadImageM(fn)
-            self.new_image = True
-            #otherwise, we don't get a new image
+    def updateCommand(self, heliStr):
+        commands = heliStr.split()
 
     def folderName(self):
         x = self.location[0]
@@ -95,12 +65,49 @@ class DroneEmulator:
         else:
             return '%s/%i-%i-%i/%i.png' %(self.baseImageDir, x, y, z,self.imageHour)
 
+    def publishImage(self):
+        self.image = cv.LoadImageM(self.folderName())
+        self.imagePublisher.publish(self.bridge.cv_to_imgmsm(self.image,"bgr8"))
+
+    def publishNavData(self):
+        z = self.location[2]
+        if self.imageHour > self.numHours/2.0:
+            imHour = self.imageHour - self.numHours
+        else:
+            imHour = self.imageHour
+        self.navData.psi = imHour/(self.numHours) * 360 * 1000 #The real navData reports in degrees times one thousand.
+        self.navData.altitude = z*self.imScale
+        self.navData.vx = self.vel[0]
+        self.navData.vy = self.vel[1]
+        self.navData.vz = self.vel[2]
+        self.navPublisher.publish(self.navData)
+
+    def hovering(self):
+        return (self.vel == (0,0,0,0))
+
+    def mainLoop(self):
+        while True:
+            if (self.hovering):
+                self.lastUpdateTime = time.time()
+            elif (time.time() - self.delayTime > self.lastUpdateTime):
+                for i in xrange(3):
+                    if self.vel[i] != 0:
+                        self.location[i] += math.copysign(1, self.vel[i])
+                if self.vel[3] != 0:
+                    self.imageHour += (math.copysign(1,self.vel[3]) % self.numHours)
+                self.lastUpdateTime = time.time()
+            #Now to publish the data like real drone.
+            self.publishImage()
+            self.publishNavData()
+
 def main(argTuple):
     rospy.init_node('ardrone_emulator')
 
     emulator = DroneEmulator(*argTuple)
 
     service = rospy.Service('droneControl', DroneControl, emulator.updateCommand)
+
+    emulator.mainLoop()
 
 if __name__ == "__main__":
     if (len(sys.argv) < 6) or (len(sys.argv) > 9):
