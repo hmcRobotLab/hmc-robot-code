21aeaf901a47e614b7d03adc6719479413d5430b
chert@robotLab <chert@robotLab>
2012-06-08 14:18:08 -0700

    Added Daisy's file to summer

---
+++ summer/lab10/heliImages.py
@@ -0,0 +1,298 @@
+#Application Specific Imports
+#from ardrone_mudd.srv import *  #Might not need this.
+#from ardrone_mudd.msg import *  #Might not need this.
+import roslib; roslib.load_manifest('ardrone_mudd')
+
+USE_DRONE = True
+IMAGE_SOURCE = "droneImage"
+
+#Generic Imports
+from std_msgs.msg import String
+from sensor_msgs.msg import *
+import rospy
+import time,sys,random,cv,cv_bridge,math,os
+import threading
+
+class ImageProcessor:
+
+  def __init__(self,use_drone = USE_DRONE):
+    #Start the open CV window thread (may not be necessary)
+    #cv.StartWindowThread()
+
+    #Setting up the publisher to broadcast the data.
+    self.publisher = rospy.Publisher('imageData', String)
+
+    #This is for when you might want to use static image sources, but it's
+    # not implemented yet. 
+    self.use_drone = use_drone
+
+    self.thresholds = {'low_red': 0, 'high_red': 256,\
+                       'low_green': 0, 'high_green': 256, \
+                       'low_blue': 0, 'high_blue':256, \
+                       'low_hue': 0, 'high_hue': 256,\
+                       'low_sat': 0, 'high_sat': 256, \
+                       'low_val': 0, 'high_val': 256}
+    self.load_thresholds()
+
+    cv.NamedWindow('image')
+    cv.MoveWindow('image', 0, 0)
+    cv.NamedWindow('threshold')
+    cv.MoveWindow('threshold',0,400)
+    self.make_control_window()
+    self.bridge = cv_bridge.CvBridge()
+    self.color_image = None             # the image from the drone
+    self.new_image = False              # did we just receive a new image?
+    self.threshed_image = None          # thresholded image
+    self.font = cv.InitFont(cv.CV_FONT_HERSHEY_PLAIN, 1, 1, 0, 1)
+    self.contours_found = False
+
+  def load_thresholds(self):
+    """ loads the thresholds from data.txt into self.thresholds """
+    try:
+      f = open("data.txt","r")
+      data = f.read()
+      f.close()
+      self.thresholds = eval(data)
+      print "thresholds loaded from data.txt"
+      self.make_control_window()
+    except:
+      print "An error occurred in loading data.txt"
+      print "Check if it's there and if it contains"
+      print " a dictionary of thresholds..."
+
+  def make_control_window(self):
+    """ a method to make a window full of sliders """
+    
+    #Create slider window
+    cv.NamedWindow('sliders')
+    cv.MoveWindow('sliders', 742, 100)
+    
+    #Create sliders
+    cv.CreateTrackbar('low_red', 'sliders', self.thresholds['low_red'], 256, self.change_low_red)
+    cv.CreateTrackbar('high_red', 'sliders', self.thresholds['high_red'], 256, self.change_high_red)
+    cv.CreateTrackbar('low_green', 'sliders', self.thresholds['low_green'], 256, self.change_low_green)
+    cv.CreateTrackbar('high_green', 'sliders', self.thresholds['high_green'], 256, self.change_high_green)
+    cv.CreateTrackbar('low_blue', 'sliders', self.thresholds['low_blue'], 256, self.change_low_blue)
+    cv.CreateTrackbar('high_blue', 'sliders', self.thresholds['high_blue'], 256, self.change_high_blue)
+    cv.CreateTrackbar('low_hue', 'sliders', self.thresholds['low_hue'], 256, self.change_low_hue)
+    cv.CreateTrackbar('high_hue', 'sliders', self.thresholds['high_hue'], 256, self.change_high_hue)
+    cv.CreateTrackbar('low_sat', 'sliders', self.thresholds['low_sat'], 256, self.change_low_sat)
+    cv.CreateTrackbar('high_sat', 'sliders', self.thresholds['high_sat'], 256, self.change_high_sat)
+    cv.CreateTrackbar('low_val', 'sliders', self.thresholds['low_val'], 256, self.change_low_val)
+    cv.CreateTrackbar('high_val', 'sliders', self.thresholds['high_val'], 256, self.change_high_val)
+
+  #Functions for changing the slider values  
+  def change_low_red(self, new_threshold):
+    self.thresholds['low_red'] = new_threshold
+  def change_high_red(self, new_threshold):
+    self.thresholds['high_red'] = new_threshold
+  def change_low_green(self, new_threshold):
+    self.thresholds['low_green'] = new_threshold
+  def change_high_green(self, new_threshold):
+    self.thresholds['high_green'] = new_threshold
+  def change_low_blue(self, new_threshold):
+    self.thresholds['low_blue'] = new_threshold
+  def change_high_blue(self, new_threshold):
+    self.thresholds['high_blue'] = new_threshold
+  def change_low_hue(self, new_threshold):
+    self.thresholds['low_hue'] = new_threshold
+  def change_high_hue(self, new_threshold):
+    self.thresholds['high_hue'] = new_threshold
+  def change_low_sat(self, new_threshold):
+    self.thresholds['low_sat'] = new_threshold
+  def change_high_sat(self, new_threshold):
+    self.thresholds['high_sat'] = new_threshold
+  def change_low_val(self, new_threshold):
+    self.thresholds['low_val'] = new_threshold
+  def change_high_val(self, new_threshold):
+    self.thresholds['high_val'] = new_threshold
+
+  def create_all_images(self):
+    """ a one-time method that creates all of the images needed
+        for the image processing...
+        
+        This should be called only if self.color_image is NOT None
+        but self.threshed_image IS None
+    """
+    #Find the size of the images
+    self.size = cv.GetSize(self.color_image)
+
+    self.red = cv.CreateImage(self.size, 8, 1)     # color components
+    self.green = cv.CreateImage(self.size, 8, 1)
+    self.blue = cv.CreateImage(self.size, 8, 1)
+
+    self.hsv = cv.CreateImage(self.size, 8, 3)     # HSV image
+    self.hue = cv.CreateImage(self.size, 8, 1)     # and its components
+    self.sat = cv.CreateImage(self.size, 8, 1)
+    self.val = cv.CreateImage(self.size, 8, 1)
+
+    self.threshed_image = cv.CreateImage(self.size, 8, 1)  # output image
+    self.copy = cv.CreateImage(self.size, 8, 1)
+
+    self.storage = cv.CreateMemStorage(0) # create memory storage for contours
+
+  def save_thresholds(self):
+    """ saves the current thresholds to data.txt """
+    f = open("data.txt","w")
+    print >> f, self.thresholds
+    f.close()
+    print "thresholds saved to data.txt"
+
+  def videoUpdate(self, data):
+    """Displays the image, calls find_info"""
+    # get the image from the Kinect, if self.use_drone == True
+    # kinect images: self.image = self.bridge.imgmsg_to_cv(data, "32FC1")
+    # drone images:
+    if self.use_drone:
+      self.color_image = self.bridge.imgmsg_to_cv(data, "bgr8")
+      self.new_image = True
+    # otherwise, get an image from file every so often
+    else:
+      cur_time = time.time()
+      if cur_time - self.last_image_time > 0.25: # number of seconds per update
+        self.last_image_time = cur_time # reset the last image time
+        folder = self.folder_names[self.location]
+        fn = folder + "/" + str(self.image_hour) + ".png"
+        #print "filename", fn
+        self.color_image=cv.LoadImageM(fn)
+        self.new_image = True
+      # otherwise, we don't get a new image
+      
+      # in case we want randomness:
+      #image_hour_number = random.randint(0,23)
+      #self.image_hour = random.randint(3,3)
+
+  # do all of the image processing
+  def process_Image(self):
+    """ here is where the image should be processed to get the bounding box """
+    # check if we've created the supporting images yet
+    if self.threshed_image == None:
+      if self.color_image != None:
+        self.create_all_images()
+
+    # from the old method call def threshold_image(self):
+    cv.Split(self.color_image, self.blue, self.green, self.red, None)
+    cv.CvtColor(self.color_image, self.hsv, cv.CV_RGB2HSV)
+    cv.Split(self.hsv, self.hue, self.sat, self.val, None)
+
+    # replace each channel with its thresholded version
+    cv.InRangeS(self.red, self.thresholds['low_red'],\
+                self.thresholds['high_red'], self.red)
+    cv.InRangeS(self.green, self.thresholds['low_green'],\
+                self.thresholds['high_green'], self.green)
+    cv.InRangeS(self.blue, self.thresholds['low_blue'],\
+                self.thresholds['high_blue'], self.blue)
+    cv.InRangeS(self.hue, self.thresholds['low_hue'],\
+                self.thresholds['high_hue'], self.hue)
+    cv.InRangeS(self.sat, self.thresholds['low_sat'],\
+                self.thresholds['high_sat'], self.sat)
+    cv.InRangeS(self.val, self.thresholds['low_val'],\
+                self.thresholds['high_val'], self.val)
+
+    # AND (multiply) all the thresholded images into one "output" image,
+    # named self.copy
+    cv.Mul(self.red, self.green, self.copy)
+    cv.Mul(self.copy, self.blue, self.copy)
+    cv.Mul(self.copy, self.hue, self.copy)
+    cv.Mul(self.copy, self.sat, self.copy)
+    cv.Mul(self.copy, self.val, self.copy)
+    # erode and dilate shave off and add edge pixels respectively
+    cv.Erode(self.copy, self.copy, iterations = 1)
+    cv.Dilate(self.copy, self.copy, iterations = 1)
+    cv.Copy(self.copy,self.threshed_image)
+
+    self.find_biggest_region()
+
+  def find_biggest_region(self):
+    """ this code should find the biggest region and
+        then determine some of its characteristics, which
+        will help direct the drone
+    """
+    # copy the thresholded image
+    cv.Copy( self.threshed_image, self.copy )  # copy self.threshed_image
+    # this is OpenCV's call to find all of the contours:
+    contours = cv.FindContours(self.copy, self.storage, cv.CV_RETR_EXTERNAL,
+                               cv.CV_CHAIN_APPROX_SIMPLE)
+
+    # Next we want to find the *largest* contour
+    if len(contours)>0:
+      biggest = contours
+      biggestArea=cv.ContourArea(contours)
+      while contours != None:
+        nextArea=cv.ContourArea(contours)
+        if biggestArea < nextArea:
+          biggest = contours
+          biggestArea = nextArea
+        contours=contours.h_next()
+    
+      #Use OpenCV to get a bounding rectangle for the largest contour
+      br = cv.BoundingRect(biggest,update=0)
+
+      #Extract the characteristics of the bounding box.
+      xl=br[0]
+      xr=xl + br[2]
+      yt=br[1]
+      yb=yt + br[3]
+
+      #Draw a contour around the bounding box.
+      cv.PolyLine(self.color_image,[[(xl,yt),(xl,yb),(xr,yb),(xr,yt)]],10, cv.RGB(0, 0, 255))
+
+      #Publish the bounding box.
+      self.publisher.publish("%i %i %i %i" % (xl,xr,yt,yb))
+
+  # the keyboard thread is the "main" thread for this program
+  def keyboardLoop(self):
+    """ the main keypress-handling thread to control the drone """
+
+    # this is the main loop for the keyboard thread
+    #
+    # don't use 'Q', 'R', 'S', or 'T'  (they're the arrow keys)
+    #
+    count = 0
+    while True:
+      count += 1
+      # handle the image processing if we have a new Kinect image
+      if (self.new_image):# and (count % 50 == 0)):
+        self.new_image = False # until we get a new one...
+        # now, do the image processing
+        # Process the image
+        self.process_Image()
+        cv.ShowImage('image', self.color_image)
+        cv.ShowImage('threshold', self.threshed_image)
+
+      # get the next keypress
+      c = cv.WaitKey(25)&255
+
+      # handle the keypress to quit...
+      if c == ord('q') or c == 27: # the Esc key is 27
+        rospy.sleep(1.42) # wait a bit for everything to finish...
+        # it's important that the FSM not re-schedule itself further than
+        # 1 second in the future, so that a thread won't be left running
+        # after this Python program quits right here:
+        return
+
+      # saving and loading the thresholds from data.txt
+      if c == ord('$'):  # dollar sign to save (looks like an S)
+        self.save_thresholds()
+
+      if c == ord('&'):  # ampersand loads thresholds from data.txt
+        self.load_thresholds()
+
+def main():
+  rospy.init_node("image_processing")
+  iP = ImageProcessor()
+
+  #Subscribing to the video source
+  print "\r Connecting to video service"
+  rospy.Subscriber(IMAGE_SOURCE, Image, iP.videoUpdate, queue_size = 1)
+  print "\r Connected\n"
+  
+  rospy.sleep(1)
+
+  #the main loop
+  iP.keyboardLoop()
+
+  print "Exiting Program"
+
+if __name__ == "__main__":
+  main()
---
+++ summer/lab10/lab10.py
@@ -0,0 +1,375 @@
+USE_DRONE = True
+
+if(USE_DRONE):
+  import roslib; roslib.load_manifest('ardrone_mudd')
+  from ardrone_mudd.srv import *
+  from ardrone_mudd.msg import *
+  from sensor_msgs.msg import *
+  import rospy
+
+import time,sys,random,cv,cv_bridge,math
+BOUNDING_DATA_SOURCE = "imageData"
+NAV_DATA_SOURCE = "navData"
+DRONE_CONTROL_SOURCE = "droneControl"
+    
+#class DroneData:
+#  altitude = 542
+#  vx = 3
+#  vy = 4
+#  vz = 0
+#  phi = 100
+#  theta = 4242
+#  psi = 3064
+#  state = 789
+
+def makeHeliStr(flag, phi, theta,gaz,yaw):
+    return "heli %i %.3f %.3f %.3f %.3f" \
+        % (flag,phi,theta,gaz,yaw)
+
+def prettyWrite(data,lastsent,boxHeight):
+  pass
+    #sys.stdout.write("\r\t [alt: %i] \t [phi: %i psi: %i theta: %i] \t [vx: %i vy: %i vz: %i] \t [bat: %i  state: %i]              \n" \
+    #  %
+    #  (data.altitude,data.phi,data.psi,data.theta,data.vx,data.vy,data.vz,data.batLevel,data.ctrlState))
+    #sys.stdout.write("\rLast sent: %s      box: %s            \n" % (lastsent, boxHeight))
+    #sys.stdout.write("\033[A")
+    #sys.stdout.write("\033[A")
+    #sys.stdout.flush()
+
+
+def folderNames():
+    return { ('c',0):"./drone_images/c_0_Ginny",
+             ('c',4):"./drone_images/c_4_Bill",
+             ('n',3):"./drone_images/n_3_Albus",
+             ('s',0):"./drone_images/s_0_Draco",
+             ('s',4):"./drone_images/s_4_Ron",
+             ('c',1):"./drone_images/c_1_Fred",
+             ('c',5):"./drone_images/c_5_Molly",
+             ('n',0):"./drone_images/n_0_Neville",
+             ('n',4):"./drone_images/n_4_Rubeus",
+             ('s',1):"./drone_images/s_1_Sirius",
+             ('s',5):"./drone_images/s_5_Hermione",
+             ('c',2):"./drone_images/c_2_George",
+             ('c',6):"./drone_images/c_6_Arthur",
+             ('n',1):"./drone_images/n_1_Luna",
+             ('n',5):"./drone_images/n_5_Severus",
+             ('s',2):"./drone_images/s_2_Remus",
+             ('s',6):"./drone_images/s_6_Harry",
+             ('c',3):"./drone_images/c_3_Charlie",
+             ('n',2):"./drone_images/n_2_Minerva",
+             ('n',6):"./drone_images/n_6_Dobby",
+             ('s',3):"./drone_images/s_3_Fawkes"}
+
+
+class DroneController:
+    """
+    The drone is keyboard-controlled for movement. Clicking in the image
+    window will give the RGB values at that point.
+    """
+
+    def __init__(self, use_drone = False):
+        """ constructor; setting up data """
+
+        # member variables relating to the drone's control
+        self.use_drone = use_drone  # are we using the drone?
+        if self.use_drone:
+            self.navDataSource = NAV_DATA_SOURCE
+            self.boundingDataSource = BOUNDING_DATA_SOURCE
+
+
+
+
+            self.droneControlSource = DRONE_CONTROL_SOURCE
+            self.init_drone_parameters()
+        else:
+            print "Not Using Drone"
+
+        self.airborne = False         # not airborne yet
+        self.run_without_data_stream = False
+        self.battery_level = "Not read from drone"
+        self.boxHeight = 0
+        self.boxX = 0
+        self.tarHeight = 35
+        self.tarX = 150
+
+        # variables for the off-board images
+        self.location = ('c',1) # the starting location (always a tuple)
+        #self.location = ( random.choice(['c','n','s']), random.randint(0,6) )
+        self.image_hour = 3 # heading at 3 o'clock == toward the markers
+        #self.angle_deg = random.randint(180,360) # the angle it's facing
+        self.last_image_time = time.time() # last time an image was grabbed
+        self.folder_names = folderNames()
+        # images and other data for the images/windows
+        self.bridge = cv_bridge.CvBridge()  # the interface to OpenCV
+
+        # variables for state machine
+        self.state = "Keyboard"
+        self.lastHover = 0
+
+    def init_drone_parameters(self):
+        """ drone-specific parameters: speed, camera, etc. """
+        print "Constructing the drone software object..."
+        cv.NamedWindow('control')
+        cv.MoveWindow('control', 200, 500)
+        
+        print "Connecting to droneControl service"
+        rospy.wait_for_service("droneControl")
+        self.heli = rospy.ServiceProxy("droneControl", Control, persistent=True)
+        
+        print "\r Connecting to navData service"
+        rospy.Subscriber(self.navDataSource,navData, self.navDataUpdate, queue_size=1)
+
+        print "\r Connecting to bounding box data"
+        rospy.Subscriber(self.boundingDataSource,std_msgs.msg.String, self.bBoxUpdate, queue_size=1)
+
+        print "\r Connected to services\n"
+        self.send("reset")
+
+        print "Drone initialized."
+
+
+    def change_location(self,dir4868ection):
+        """ changes the location from which an image is taken in one of six ways """
+        # get the state
+        current_ns_character = self.location[0]
+        current_ew_number = self.location[1]
+        current_image_hour = self.image_hour
+
+        # make appropriate changes
+        # reset the state
+        self.location = (current_ns_character,current_ew_number)
+        self.image_hour = current_image_hour
+        print "location and image_hour are now", self.location, self.image_hour
+
+    def navDataUpdate(self, data):
+        prettyWrite(data,self.lastsent,self.boxHeight)
+
+    def bBoxUpdate(self, data):
+        br = data.data.split()
+        xl = int(br[0])
+        xr = int(br[1])
+        yt = int(br[2])
+        yb = int(br[3])
+        if (xr-xl)*(yb-yt) < 200:
+            self.boxX = float("inf")
+        else:
+            self.boxX = (xl + xr)/2
+            self.boxHeight = yb - yt #the y axis points down.
+
+    # put text on the image...
+    def text_to_image(self):
+        """ write various things on the image ... """
+        # the image is 320 pixels wide and 240 pixels high
+        # clear a rectangle
+        cv.Rectangle(self.color_4868image, (3,3), (242,30),
+                     cv.RGB(255,255,255), cv.CV_FILLED )
+                    
+        # set up some text           
+        llx = 7
+        lly = 22
+        s = "State: " + str(self.state)
+        textllpoint = (llx,lly)
+        cv.PutText(self.color_image, s, textllpoint, self.font, cv.RGB(0,0,255))
+
+    # our drone's state machine
+    def FSM1(self, timer_event=None):
+        """ the finite-state machine -- DAISY YOUR EMAIL HAS THE PART FOR THE IMAGE!!! """
+        print "Now in state", self.state
+
+        if self.state == "Keyboard": # user stopped our state machine!
+          ##self.send("heli 1 0 0 0 0")
+          ##rospy.sleep(4.0)
+          self.send("land")
+          self.state = "Stopped"
+          return
+
+        elif self.state == "takeoff":
+          rospy.sleep(1.0)
+          self.send("takeoff")
+          rospy.sleep(4.0)
+          self.state = "hover"
+
+        elif self.state == "hover":
+         self.send(makeHeliStr(0,0,0,0,0))
+         rospy.sleep(1.0)
+         self.state = "looking"
+
+        elif self.state == "looking":
+          print self.boxX
+          if abs(self.tarX-self.boxX) < 40:
+            self.state = "approach"
+          elif self.boxX < self.tarX:
+            pass
+            self.send("heli 0 0 0 0 -.15")
+          else:
+            pass
+            self.send ("heli 0 0 0 0 .15")
+
+        elif self.state == "approach":
+            if abs(self.boxX - self.tarX) > 40:
+                self.state = "looking"
+            elif self.tarHeight - self.boxHeight > 10:
+                self.send(makeHeliStr(1,0,-.10,0,0))
+                rospy.sleep(.025)
+            else:
+                self.send(makeHeliStr(0,0,0,0,0))
+                rospy.sleep(.25)
+                self.state = "Keyboard"
+
+        if self.state != "Stopped":
+          rospy.Timer( rospy.Duration(0.1), self.FSM1, oneshot=True )
+            
+
+                
+    # the keyboard thread is the "main" thread for this program
+    def keyboardLoop(self):
+        """ the main keypress-handling thread to control the drone """
+        maxpower = .15
+
+        # this is the main loop for the keyboard thread
+        #
+        # don't use 'Q', 'R', 'S', or 'T'  (they're the arrow keys)
+        #
+        while True:
+
+            #if self.use_drone == False: # if no drone, we get our own images
+            #    #Add in non drone control.
+            # get the next keypress
+
+            c = chr(cv.WaitKey()&255)
+
+            # handle the keypress to quit...
+            if c == 'q' or c == chr(27): # the Esc key is 27
+                self.state = "Keyboard" # stop the FSM
+                self.send( "land" ) # lands and halts
+                rospy.sleep(1.42) # wait a bit for everything to finish...
+                # it's important that the FSM not re-schedule itself further than
+                # 1 second in the future, so that a thread won't be left running
+                # after this Python program quits right here:
+                return
+
+
+            if c == 'f':  # capital-F starts and stops the finite-state machine
+                # only run it when it's not already running!
+                if self.state == "Keyboard":  # it's not yet running, so
+                    print "Starting state machine..."
+                    self.state = "takeoff"  # these two lines start FSM1()
+                    self.FSM1()
+                # if it's already running, stop it!
+                else:  # if it's in any other state, we bring it back to "Keyboard"
+                    self.state = "Keyboard" # should stop the state machine
+                    print "Stopping state machine..."
+
+            if c == ',':
+                self.saw_capital_c = True
+
+
+            # here are the keyboard commands for the drone...
+            #
+            # ** NEW THIS WEEK **   you must hit the space bar to stop a motion
+            #
+            # that is, the space bar hovers; it no longer automatically hovers...
+
+            helistr= ""
+            if c == '\n':
+                self.state = "keyboard"
+                helistr = "land"
+            elif c == 't': 
+                helistr = "takeoff"
+            elif c == ' ':  # the space bar will hover
+
+                helistr = "heli 0 0 0 0 0"
+            elif self.airborne:
+                if char == ord('w'):
+                    sflag = 1
+                    stheta = -maxpower
+                elif char == ord('x'):
+                    sflag = 1
+                    stheta = maxpower
+                elif char == ord('a'):
+
+                    sflag = 1
+                    sphi = -maxpower  
+                elif char == ord('d'):
+                    sflag = 1
+                    sphi = maxpower  
+                elif char == ord('e'):
+
+                    sflag = 1
+                    stheta = -math.sqrt(maxpower/2)
+                    sphi = math.sqrt(maxpower/2)  
+                elif char == ord('z'):
+                    sflag = 1
+                    stheta = math.sqrt(maxpower/2)
+                    sphi = -math.sqrt(maxpower/2)  
+                elif char == ord('q'):
+                    sflag = 1
+                    stheta = -math.sqrt(maxpower/2)
+                    sphi = -math.sqrt(maxpower/2)
+
+  
+                elif char == ord('c'):
+                    sflag = 1
+                    stheta = math.sqrt(maxpower/2)
+                    sphi = math.sqrt(maxpower/2)  
+                elif char == ord('s'):
+                    self.send("heli 1 0 0 0 0")
+                    rospy.sleep(.05)
+                    sflag = 0
+                else:
+                    self.send(self.lastsent)
+                
+                helistr = makeHeliStr(sflag,sphi,stheta,sgaz,syaw)
+            if not (helistr == ""):
+                self.send(helistr)
+
+
+            # if self.use_drone == False (simulated mode)
+            # the arrow keys allow you to change the images you see...
+            if not self.use_drone:
+                if c == 82:  # up arrow is the same as ord('R')
+                    self.change_location( 'west' )
+                if c == 84:  # down arrow is the same as ord('T')
+                    self.change_location( 'east' )
+                if c == 81:  # left arrow is the same as ord('Q')
+                    self.change_location( 'north' )
+                if c == 83:  # right arrow is the same as ord('S')
+                    self.change_location( 'south' )
+                if c == ord('-'):  # '-' decreases the image_hour
+                    self.change_location( 'counterclockwise' )
+                if c == ord('=') or c == ord('+'):  # '=' or '+' increases the image_hour
+                    self.change_location( 'clockwise' )
+
+    def send(self,command):
+        self.lastsent = command
+        self.heli(command)    
+    
+if __name__ == '__main__':
+    '''
+    Main driver function for the drone.
+    '''
+    # Setup stuff
+    rospy.init_node('dronekinect')  # names this ROS node
+
+    use_drone = USE_DRONE
+    controller = DroneController(use_drone)
+
+    # Display initial message
+    print 'Keyboard controls:\n'
+    print 't takes off'
+    print '\\n (return/enter) lands the drone'
+    print 'space bar hovers (stops the drone\'s motion)\n'
+    print 'The qweasdzxc keys translate at a fixed height and orientation'
+    print 'i/k move the drone up/down'
+    print 'j/l turn the drone left/right\n'
+    print 'q or esc lands the drone and quits the program.'
+
+    rospy.sleep(1)
+    
+    # start the main loop, the keyboard thread:
+    controller.keyboardLoop()
+    
+    print "the Python program is quitting..."
+       
+if __name__ == "__main__":
+  main()
