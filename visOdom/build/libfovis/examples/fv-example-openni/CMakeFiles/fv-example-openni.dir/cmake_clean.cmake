0d27e5fb7deb8a7d4700dee38c50c18abf30bb37
arnold <arnold@robotLab>
2012-06-21 17:22:24 -0700

    Now it works fast after a 40 second compilation from rosmake

---
+++ visOdom/build/libfovis/examples/fv-example-openni/CMakeFiles/fv-example-openni.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/fv-example-openni.dir/main.cpp.o"
+  "CMakeFiles/fv-example-openni.dir/data_capture.cpp.o"
+  "../../../bin/fv-example-openni.pdb"
+  "../../../bin/fv-example-openni"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/fv-example-openni.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ visOdom/build/libfovis/testers/CMakeFiles/absolute-orientation-tester.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/robotics/ros_workspace/hmc-robot-code/visOdom/libfovis/testers/absolute_orientation_horn_tester.cpp" "/home/robotics/ros_workspace/hmc-robot-code/visOdom/build/libfovis/testers/CMakeFiles/absolute-orientation-tester.dir/absolute_orientation_horn_tester.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
